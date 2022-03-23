47bae6d216f54e60f73105b922f2ddfd8057576e
aqillakhamis <aqillakhamis@gmail.com>
2022-03-23 11:11:04 +0800

    tutorial week 1

---
+++ hello-world.c
@@ -0,0 +1,14 @@
+//documentation section
+/*this is your first tutorial submission using GitHub and produce required output*/
+
+//pre-processor section
+#include<stdio.h>
+
+//global variable section
+
+//main function section 
+int main(){
+
+}
+
+//user-defined section
